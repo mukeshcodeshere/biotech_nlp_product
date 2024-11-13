5f223c2fbdd710a8f1480305834984c63d6906b3
Mukeshwaran Baskaran <mukesh@adarone.com>
2024-11-13 15:35:41 -0600

    close

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ CONFIG = {
     'END_DATE': '2024-11-01',
     'BASE_DIR': 'sec_data',
     'USER_AGENT': 'Your Name your@email.com',
-    'DATABASE_URL': 'postgresql://db_biotech_owner:t0KZ7pnguiFb@ep-shrill-smoke-a5xu8h2u.us-east-2.aws.neon.tech/db_biotech?sslmode=require'
+    'DATABASE_URL': 'FALSE'
 }
