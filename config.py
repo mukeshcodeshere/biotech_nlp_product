602c338fd86f0f79ac865a4654a3184514a3cbbc
Mukeshwaran Baskaran <mukesh@adarone.com>
2024-11-13 10:04:05 -0600

    updated

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+CONFIG = {
+    'TICKERS': ['AAPL', 'MSFT', 'GOOGL'],
+    'START_DATE': '2024-01-01',
+    'END_DATE': '2024-06-01',
+    'BASE_DIR': 'sec_data',
+    'USER_AGENT': 'Your Name your@email.com'
+}
