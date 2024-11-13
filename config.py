6259629cbb623a39591122555bdad725b744fd86
Mukeshwaran Baskaran <mukesh@adarone.com>
2024-11-13 14:06:35 -0600

    sec_processor.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 CONFIG = {
     'TICKERS': ['VRTX', 'REGN'],
     'START_DATE': '2024-01-01',
-    'END_DATE': '2024-06-01',
+    'END_DATE': '2024-11-01',
     'BASE_DIR': 'sec_data',
     'USER_AGENT': 'Your Name your@email.com'
 }
